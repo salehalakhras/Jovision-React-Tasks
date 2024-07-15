dc496f01d08ac52026ff9ec3109b63eb095208d4
Saleh Alakhras <salehalakhras2000@gmail.com>
2024-07-15 13:44:47 +0300

    finished task 42

--- reactApp/Components/NavigationFComponent_Task41.js
+++ reactApp/Components/NavigationFComponent_Task41.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { StyleSheet, View,Text } from 'react-native';
+import { StyleSheet, View,Text,Button } from 'react-native';
 
 
 export const Screnn1 = ({navigation}) => {
     return (
         <View style={styles.container}>
             <Text style={{fontSize:24}}>{'Screen' + (navigation.getState().index + 1)}</Text>
+            <Button title={'Screen 2'} onPress={()=>{navigation.navigate('Screen 2')}}></Button>
+            <Button title={'Screen 3'} onPress={()=>{navigation.navigate('Screen 3')}}></Button>
+            <Button title={'Screen 4'} onPress={()=>{navigation.navigate('Screen 4')}}></Button>
         </View>
     );
 }
@@ -14,6 +17,9 @@ export const Screnn2 = ({navigation}) => {
     return (
         <View style={styles.container}>
              <Text style={{fontSize:24}}>{'Screen' + (navigation.getState().index + 1)}</Text>
+             <Button title={'Screen 1'} onPress={()=>{navigation.navigate('Screen 1')}}></Button>
+            <Button title={'Screen 3'} onPress={()=>{navigation.navigate('Screen 3')}}></Button>
+            <Button title={'Screen 4'} onPress={()=>{navigation.navigate('Screen 4')}}></Button>
         </View>
     );
 }
@@ -21,6 +27,9 @@ export const Screnn3 = ({navigation}) => {
     return (
         <View style={styles.container}>
             <Text style={{fontSize:24}}>{'Screen' + (navigation.getState().index + 1)}</Text>
+            <Button title={'Screen 1'} onPress={()=>{navigation.navigate('Screen 1')}}></Button>
+            <Button title={'Screen 2'} onPress={()=>{navigation.navigate('Screen 2')}}></Button>
+            <Button title={'Screen 4'} onPress={()=>{navigation.navigate('Screen 4')}}></Button>
         </View>
     );
 }
@@ -28,6 +37,9 @@ export const Screnn4 = ({navigation}) => {
     return (
         <View style={styles.container}>
              <Text style={{fontSize:24}}>{'Screen' + (navigation.getState().index + 1)}</Text>
+             <Button title={'Screen 1'} onPress={()=>{navigation.navigate('Screen 1')}}></Button>
+            <Button title={'Screen 2'} onPress={()=>{navigation.navigate('Screen 2')}}></Button>
+            <Button title={'Screen 3'} onPress={()=>{navigation.navigate('Screen 3')}}></Button>
         </View>
     );
 }
@@ -36,7 +48,7 @@ const styles = StyleSheet.create({
     container:{
         flex:1,
         justifyContent: 'center',
-        alignItems: 'center',
+
     }
 })
 
